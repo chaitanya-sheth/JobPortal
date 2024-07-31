c4f782e199179e189e4beacc7b8a4c39519c3eb0
Chaitanya Sheth <chaitanya.sheth@abjayon.com>
2024-07-31 15:38:18 +0530

    Registration Page is created

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React,{useState} from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
 
-export default Register
+  const [name,setName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState(""); 
+
+  const handleSubmit = (e) => {
+    try{
+      e.preventDefault()
+      console.log(name,lastName,email,password)
+    }
+    catch(error){
+      console.log(error)
+    }
+    
+  }
+  return (
+    <>
+    <div className="form-container">
+      <form className="card p-4" onSubmit={handleSubmit}>
+      <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="location" className="form-label">
+            Location
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="location"
+          />
+        </div>
+        
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+      </form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
